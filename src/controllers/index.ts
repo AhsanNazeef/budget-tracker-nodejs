59d389fd54ea4cecc715da0034a4125385ad77c6
Ahsan Nazeef <ahsannazeefahmed1141@gmail.com>
2024-11-29 18:01:20 +0500

    refactored code

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,12 @@
 import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 import * as homeServices from "../services";
-import { CustomApiRequest } from "common/interfaces";
+import { CustomApiRequest } from "../common/interfaces";
+import { createSuccessResponse } from "../common/helpers";
 
 export const helloFromServer = async (req: CustomApiRequest, res: Response) => {
   const data = homeServices.helloFromServer();
-  res.json(data);
+  res.statusCode = StatusCodes.OK;
+  res.send(createSuccessResponse(data));
 };
